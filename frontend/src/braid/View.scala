b27300f681d2b4c6537c7efa74d883ef13927d16
Pavel <pavel.oliynyk@gmail.com>
2025-12-03 19:59:46 +0000

    feat: compose drop with sending event to the server side;

--- frontend/src/braid/View.scala
+++ frontend/src/braid/View.scala
@@ -105,10 +105,12 @@ class View(controller: Controller) {
         className := "px-4 py-4 text-center",
         button(
           "Delete",
-          onClick --> controller.dropSingleRow(habit.id),
-//          onClick.flatMap(_ => FetchStream.post("/habit/" + habit.id)) --> {
-//            responseText => println(responseText)
-//          },
+          onClick.flatMap(_ => FetchStream.post("/habit/" + habit.id)) --> {
+            responseText => println(responseText)
+          },
+          inContext(_.events(onClick).throttle(3000) --> {
+            controller.dropSingleRow(habit.id)
+          }),
           className := "text-red-500 hover:text-red-700 transition"
         )
       )
